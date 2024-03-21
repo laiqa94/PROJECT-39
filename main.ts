baeda24d26053f0ca14714ca9d9d5ae0a40f5b93
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 03:04:54 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.ts
@@ -0,0 +1,12 @@
+
+
+function city_country(city:string, country:string):string {
+
+    return `${city}, ${country}`;
+}
+
+console.log(city_country('karachi','Pakistan'));
+
+console.log(city_country('Tokyo','Japan'));
+
+console.log(city_country('Paris','France'));
